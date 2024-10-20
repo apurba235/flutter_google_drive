a4e23b8284b26931be89e849c34b3e8e33774349
apurbanag <apurba235nag@gmail.com>
2024-10-20 13:28:36 +0530

    export done

--- lib/flutter_google_drive.dart
+++ lib/flutter_google_drive.dart
@@ -1,4 +1,9 @@
 library flutter_google_drive;
-
-export 'package:flutter_google_drive/src/show_files.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_google_drive/src/circular_progress_indicator_with_blur.dart';
+import 'package:flutter_google_drive/src/drive_operations.dart';
+import 'package:flutter_google_drive/src/mime_type.dart';
+import 'package:googleapis/drive/v3.dart';
+import 'dart:io' as io;
+part 'show_files.dart';
 
--- lib/show_files.dart
+++ lib/show_files.dart
@@ -1,9 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_google_drive/src/circular_progress_indicator_with_blur.dart';
-import 'package:flutter_google_drive/src/drive_operations.dart';
-import 'package:flutter_google_drive/src/mime_type.dart';
-import 'package:googleapis/drive/v3.dart';
-import 'dart:io' as io;
+
+
+part of 'flutter_google_drive.dart';
 
 class ShowFiles extends StatefulWidget {
   const ShowFiles({super.key});
